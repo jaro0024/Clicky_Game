cbe74d677f1f0414def55f880bd1bfde69bc5397
daniela jaros <danielajaros@gmail.com>
2018-04-22 15:16:37 -0400

    app is working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,116 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import ImageCard from "./components/ImageCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./components/Container";
+import Column from "./components/Column";
+import Row from "./components/Row";
+import images from "./images.json";
 import './App.css';
 
+// Shuffle images
+function randomImages(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class App extends Component {
+
+  state = {
+    images,
+    currentScore: 0,
+    topScore: 0,
+    guessStatus: "",
+    clicked: []
+  }
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      guessStatus: "You guessed correctly!"
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ guessStatus: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      guessStatus: "You guessed incorrectly!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledImages = randomImages(images);
+    this.setState({ images: shuffledImages });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <Wrapper>
+
+        <Nav
+          title="Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          guessStatus={this.state.guessStatus}
+        />
+
+        <Title>
+          Click on an image to earn points! You will loose if you click on any images more than once!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.images.map(gameImage => (
+              <Column size="md-3 sm-6">
+                <ImageCard
+                  key={gameImage.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={gameImage.id}
+                  image={gameImage.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+
+      </Wrapper>
     );
   }
 }
